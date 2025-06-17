dabd758e661226e8b3747ce17490dd31bd9f18e0
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2025-06-18 00:33:57 +0100

    installing filament

---
+++ vite.config.filament.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/filament/admin/theme.css'],
+            refresh: true,
+        }),
+    ],
+    css: {
+        postcss: {
+            from:'./postcss.config..filament.js',
+            plugins: [
+                tailwindcss(),
+                autoprefixer(),
+            ],
+        },
+    },
+    build: {
+        outDir: 'public/build/filament',
+    },
+});
