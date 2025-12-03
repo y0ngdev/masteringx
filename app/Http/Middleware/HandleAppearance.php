86435ee0a33b57059270eda0087b14abaeefedfc
Prosper Ugbovo <prosperugbovo@gmail.com>
2025-12-03 15:40:06 +0100

    Update HandleAppearance.php

--- app/Http/Middleware/HandleAppearance.php
+++ app/Http/Middleware/HandleAppearance.php
@@ -16,8 +16,8 @@ class HandleAppearance
      */
     public function handle(Request $request, Closure $next): Response
     {
-        //        View::share('appearance', $request->cookie('appearance') ?? 'system');
-        View::share('appearance', 'light');
+               View::share('appearance', $request->cookie('appearance') ?? 'light');
+        // View::share('appearance', 'light');
 
         return $next($request);
     }
