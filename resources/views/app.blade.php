ce398858cb04e3abfb46b6c6a6c5fb3fa54a0db4
Prosper Ugbovo <prosperugbovo@gmail.com>
2025-12-03 15:42:38 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  >
+    <!-- @class(['dark' => ($appearance ?? 'system') == 'dark']) -->
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <!-- <script>
+        <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
 
@@ -13,10 +14,10 @@
                     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
                     if (prefersDark) {
-                        document.documentElement.classList.add('dark');
+                        document.documentElement.classList.add('light');
                     }
                 }
-            })(); -->
+            })();
         </script>
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
